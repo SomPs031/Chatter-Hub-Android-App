4cdd65f990784168e79e8649797fed5e32ead138
Som Prakash Sahu <somprakashsahu09@gmail.com>
2025-01-09 18:24:07 +0530

    Files Added

---
+++ app/src/main/java/com/example/chatterHub/CAApplication.kt
@@ -0,0 +1,7 @@
+package com.example.chatterHub
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CAApplication: Application()
---
+++ app/src/main/java/com/example/chatterHub/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.example.chatterHub
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+import androidx.navigation.compose.rememberNavController
+import com.example.chatterHub.ui.ChatListScreen
+import com.example.chatterHub.ui.LoginScreen
+import com.example.chatterHub.ui.ProfileScreen
+import com.example.chatterHub.ui.SignupScreen
+import com.example.chatterHub.ui.SingleChatScreen
+import com.example.chatterHub.ui.SingleStatusScreen
+import com.example.chatterHub.ui.StatusListScreen
+import com.example.chatterHub.ui.theme.ChatAppCloneTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+sealed class DestinationScreen(val route: String) {
+    object Signup : DestinationScreen("signup")
+    object Login : DestinationScreen("login")
+    object Profile : DestinationScreen("profile")
+    object ChatList : DestinationScreen("chatList")
+    object SingleChat : DestinationScreen("singleChat/{chatId}") {
+        fun createRoute(id: String) = "singleChat/$id"
+    }
+    object StatusList : DestinationScreen("statusList")
+    object SingleStatus : DestinationScreen("singleStatus/{userId}") {
+        fun createRoute(userId: String?) = "singleStatus/$userId"
+    }
+}
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ChatAppCloneTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ChatAppNavigation()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ChatAppNavigation() {
+    val navController = rememberNavController()
+    val vm = hiltViewModel<CAViewModel>()
+
+    NotificationMessage(vm = vm)
+
+    NavHost(navController = navController, startDestination = DestinationScreen.Signup.route) {
+        composable(DestinationScreen.Signup.route) {
+            SignupScreen(navController, vm)
+        }
+        composable(DestinationScreen.Login.route) {
+            LoginScreen(navController, vm)
+        }
+        composable(DestinationScreen.Profile.route) {
+            ProfileScreen(navController = navController, vm = vm)
+        }
+        composable(DestinationScreen.StatusList.route) {
+            StatusListScreen(navController = navController, vm)
+        }
+        composable(DestinationScreen.SingleStatus.route) {
+            val userId = it.arguments?.getString("userId")
+            userId?.let {
+                SingleStatusScreen(navController = navController, vm = vm, userId = userId)
+            }
+        }
+        composable(DestinationScreen.ChatList.route) {
+            ChatListScreen(navController = navController, vm = vm)
+        }
+        composable(DestinationScreen.SingleChat.route) {
+            val chatId = it.arguments?.getString("chatId")
+            chatId?.let {
+                SingleChatScreen(navController = navController, vm = vm, chatId = it)
+            }
+        }
+    }
+}
+
+
+
+
+
